fe74e8d24dedda5140aa1ae9cf6cabbcebaaa32b
Tommaso Giovannoni <tommasogiovannoni@Air-di-Tommaso.homenet.telecomitalia.it>
2026-06-23 20:10:11 +0200

    rimosse foto autore review in home e servizi

--- resources/views/servizi.blade.php
+++ resources/views/servizi.blade.php
@@ -144,7 +144,7 @@
                         <div class="faq-answer">
                             <p>Sì, quando il progetto lo richiede è possibile prevedere stampe, album o formati specifici per comunicazione e archiviazione. La consegna viene concordata prima dello shooting.</p>
                         </div>
-                        
+
                 </div>
             </div>
         </section>
@@ -163,46 +163,34 @@
                         <div class="review-track">
                             <div class="review-card stack-xl">
                                 <h4>"Michele ha saputo metterci a nostro agio e restituire immagini naturali, curate e mai forzate."</h4>
-                                <div class="review-title">
-                                    <img src="{{ asset('images/portfolio-1.jpeg') }}" style="width: 100%;">
                                     <div>
                                         <h6>Cliente ritratto</h6>
                                         <p>Ritratti professionali</p>
                                     </div>
-                                </div>
                             </div>
 
                             <div class="review-card stack-xl">
                                 <h4>"Ha capito subito il carattere del luogo e lo ha trasformato in una serie di immagini perfette per la nostra comunicazione."</h4>
-                                <div class="review-title">
-                                    <img src="{{ asset('images/portfolio-2.jpeg') }}" style="width: 100%;">
                                     <div>
                                         <h6>Luca Moretti</h6>
                                         <p>Spazi e location</p>
                                     </div>
-                                </div>
                             </div>
 
                             <div class="review-card stack-xl">
                                 <h4>"Le foto prodotto sono finalmente pulite, credibili e riconoscibili. Il processo è stato semplice dall'inizio alla consegna."</h4>
-                                <div class="review-title">
-                                    <img src="{{ asset('images/portfolio-3.jpeg') }}" style="width: 100%;">
                                     <div>
                                         <h6>Giulia Ferri</h6>
                                         <p>Brand e prodotto</p>
                                     </div>
-                                </div>
                             </div>
 
                             <div class="review-card stack-xl">
                                 <h4>"Durante la cerimonia è stato presente senza essere invadente. La gallery finale racconta davvero l'atmosfera di quel giorno."</h4>
-                                <div class="review-title">
-                                    <img src="{{ asset('images/portfolio-4.jpeg') }}" style="width: 100%;">
                                     <div>
                                         <h6>Marco Rinaldi</h6>
                                         <p>Cerimonia privata</p>
                                     </div>
-                                </div>
                             </div>
                         </div>
                     </div>
